ca9b2a924505704ddbb72dbed32905f1916ddcaf
Flaviu Vanca <skety_tm@yahoo.com>
2025-06-26 17:27:57 +0100

    changed some imports to pass the test

--- src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/RunCucumberIntegrationTestCase.java
+++ src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/RunCucumberIntegrationTestCase.java
@@ -2,8 +2,8 @@ package com.in28minutes.microservices.currencyexchangeservice.cucumber;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, features = "src/test/resources", plugin = { "pretty" })
